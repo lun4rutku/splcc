5561e4aeefef170cbc470f638ffbf174af4c4fb1
lun4rutku <connect.sentiabot@gmail.com>
2023-04-27 18:01:26 +0300

    Added country code check to main.py

--- main.py
+++ main.py
@@ -10,10 +10,22 @@ variables = {
     "module": None
 }
 
+phonemd {
+    "supported": [
+        "+90"
+    ]
+}
+
 while True:
     input = metashell.getUserInput()
     if (variables["module"]): module = variables["module"]
     else: module = None
 
-    if (input.startswith("search")):
-        
+    if (input.startswith("search ")):
+        splittedinput = input.split()
+        if splittedinput[1] == "phone":
+            print("Searching for country code is supported.")
+            countrycode = splittedinput[2].split("--")[1]
+            if countrycode in phonemd["supported"]:
+                print("Supported country code.")
+                
